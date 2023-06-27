e588e90b6c358e897e78616d819984dc841a9e96
Thomas Hopkins <thomashopkins000@gmail.com>
2023-06-27 07:59:16 -0400

    mAP metric and some tests

---
+++ test/test_utils.py
@@ -0,0 +1,33 @@
+from torch import tensor
+from ..utils import *
+
+def test_accuracy():
+    
+    logits = tensor([[0.2, 0.8], [0.0, 1.0], [1.0, 0.0], [0.4, 0.6], [0.49999, 0.50001]], dtype=torch.float)
+    labels = tensor([1, 1, 0, 1, 1], dtype=torch.long)
+
+    assert accuracy(logits, labels) == 1.0
+
+    labels = tensor([0, 0, 1, 0, 0], dtype=torch.long)
+
+    assert accuracy(logits, labels) == 0.0
+
+
+def test_mAP_perfect_prediction():
+
+    # 5, 20x20 image masks
+    predictions = torch.zeros((3, 20, 20), dtype=torch.long)
+    targets = torch.zeros((3, 20, 20), dtype=torch.long)
+
+    # nothing to predict
+    assert mAP(predictions, targets) == 0.0
+
+    # single square
+    predictions[0, 5:10, 5:10] = 1
+    targets[0, 5:10, 5:10] = 1
+    predictions[1, 2:3, 2:3] = 1
+    targets[1, 2:3, 2:3] = 1
+
+    assert mAP(predictions, targets) == 1.0
+
+
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+from skimage import measure
 import torch
 
 def accuracy(logits, labels):
@@ -6,8 +8,57 @@ def accuracy(logits, labels):
 
 
 def get_model_gpu_memory(model):
-    dummy_input = torch.randn(10, 3, 512, 512, dtype=torch.half).cuda()
     torch.cuda.empty_cache()
     memory_allocated = torch.cuda.memory_allocated() / 1024 ** 2
     memory_cached = torch.cuda.memory_cached() / 1024 ** 2
     return memory_allocated, memory_cached
+
+
+def mAP(predictions, targets, iou_threshold=0.6):
+    '''
+    Computes the mean average precision (mAP) for an instance segmentation task.
+
+    Parameters
+    ----------
+    predictions : torch.tensor
+        Boolean prediction masks
+    targets : torch.tensor
+        Boolean ground-truth masks
+    iou_threshold : float, optional
+        Threshold at which to count predictions as positive predictions
+
+    Returns
+    -------
+    mAP : float
+        Mean average precision score
+    '''
+    true_positives = []
+    false_positives = []
+    total_targets = len(targets)
+
+    intersection = torch.logical_and(predictions, targets)
+    union = torch.logical_or(predictions, targets)
+
+    intersection_labels = measure.label(intersection)
+    union_labels = measure.label(union)
+
+    num_intersecting_regions = np.max(intersection_labels, axis=(1, 2))
+    num_union_regions = np.max(union_labels, axis=(1, 2))
+
+    ious = num_intersecting_regions / num_union_regions
+
+    for i in range(len(predictions)):
+        best_iou = np.max(ious[i])
+        best_index = np.argmax(ious[i])
+
+        if best_iou >= iou_threshold and best_index not in true_positives:
+            true_positives.append(best_index)
+        else:
+            false_positives.append(predictions[i])
+
+    precision = len(true_positives) / (len(true_positives) + len(false_positives))
+    recall = len(true_positives) / total_targets
+
+    ap = precision * recall
+
+    return ap
