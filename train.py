5588645ce0d627e039ffe8d4fc43c6d835620308
thomashopkins32 <thomashopkins000@gmail.com>
2023-07-23 14:12:25 -0400

    Adding model loading; Tested saving/loading pipeline

--- train.py
+++ train.py
@@ -20,15 +20,16 @@ MOMENTUM = 0.0
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 VALID_STEP = 5
 RNG = 32
-EPOCHS = 10
-CHECKPOINT_STEP = 5
-CHECKPOINT_LOAD_PATH = None
+EPOCH_START = 0
+EPOCH_END = 10
+CHECKPOINT_STEP = 1
+CHECKPOINT_LOAD_PATH = os.path.join('checkpoints', f'{RUN_NAME}.pt')
 CHECKPOINT_SAVE_PATH = os.path.join('checkpoints', f'{RUN_NAME}.pt')
 INCLUDE_UNSURE = True
 
 torch.manual_seed(RNG)
 
-writer = SummaryWriter(max_queue=1000, flush_secs=300)
+writer = SummaryWriter()
 dataset = HuBMAP(include_unsure=INCLUDE_UNSURE)
 generator = torch.Generator().manual_seed(RNG)
 train_data, valid_data = random_split(dataset, [0.9, 0.1], generator=generator)
@@ -38,16 +39,18 @@ model = UNet2d().to(DEVICE)
 loss_func = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LR)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max')
+if CHECKPOINT_LOAD_PATH:
+    EPOCH_START = load_model_checkpoint(CHECKPOINT_LOAD_PATH, model, optimizer, scheduler=scheduler) 
 
-for e in tqdm(range(EPOCHS)):
+for e in tqdm(range(EPOCH_START + 1, EPOCH_END + 1)):
     if e % VALID_STEP == 0:
         loss = train_one_epoch(e, model, train_loader, loss_func, optimizer, writer=writer, device=DEVICE)
         val_loss, val_metric = validate_one_epoch(e, model, valid_loader, loss_func, writer, device=DEVICE)
+        if scheduler:
+            scheduler.step(val_metric)
     else:
         loss = train_one_epoch(e, model, train_loader, loss_func, optimizer, device=DEVICE)
     if e % CHECKPOINT_STEP == 0:
-        save_model_checkpoint(CHECKPOINT_SAVE_PATH, e, model, optimizer, loss, scheduler=scheduler, device=DEVICE)
+        save_model_checkpoint(CHECKPOINT_SAVE_PATH, e, model, optimizer, loss, scheduler=scheduler)
     writer.add_scalar('gpu_memory_usage', torch.cuda.memory_allocated(DEVICE), global_step=e)
-    if scheduler:
-        scheduler.step(val_metric)
 writer.close()
