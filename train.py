Correct class weighting scheme

train.py
@@ -11,13 +11,13 @@ from utils import *
 
 # PARAMETERS
 RUN_NAME = 'testing_run'
-BATCH_SIZE = 1
+BATCH_SIZE = 4
 LR = 1e-5
 WD = 0.0
 MOMENTUM = 0.99
 DATA_TRANSFORMATIONS = True
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-VALID_STEP = 5
+VALID_STEP = 1
 RNG = 32
 EPOCH_START = 0
 EPOCH_END = 10
@@ -38,7 +38,7 @@ print(f'Calculating weight rescaling from {len(train_data)} training points')
 bv_count = 0
 glom_count = 0
 bg_count = 0
-for batch in tqdm(train_data):
+for batch in tqdm(train_loader):
     m = batch['mask']
     bv_count += torch.count_nonzero(m == 2).item()
     glom_count += torch.count_nonzero(m == 1).item()
@@ -46,9 +46,9 @@ for batch in tqdm(train_data):
 total_count = bv_count + glom_count + bg_count
 
 weight_rescale = torch.tensor([
-    bv_count / total_count,
-    glom_count / total_count,
-    bg_count / total_count
+    total_count / (3 * bg_count),
+    total_count / (3 * glom_count),
+    total_count / (3 * bv_count)
 ], dtype=torch.float, device=DEVICE)
 print(f'Class rescaling will be done based on the following: {weight_rescale}')
 

utils.py
@@ -124,6 +124,7 @@ def logits_to_blood_vessel_mask(logits):
 
 
 def train_one_epoch(epoch, model, train_loader, loss_func, optimizer, writer=None, data_transforms=False, device='cpu', **kwargs):
+    total_mAP = 0.0
     total_loss = 0.0
     data_size = len(train_loader)
     model.train()
@@ -147,10 +148,16 @@ def train_one_epoch(epoch, model, train_loader, loss_func, optimizer, writer=Non
                 predictions = logits_to_blood_vessel_mask(logits)
                 blood_vessel_gt = (y == 2).type(torch.long)
                 mAP_value = mAP(predictions, blood_vessel_gt, **kwargs)
-                writer.add_scalar("Loss/train", loss.item(), global_step=epoch)
-                writer.add_scalar("mAP/train", mAP_value, global_step=epoch)
+                total_mAP += mAP_value
     
-    return total_loss / data_size
+    avg_loss = total_loss / data_size
+    avg_mAP = total_mAP / data_size
+
+    if writer:
+        writer.add_scalar("Loss/train", avg_loss, global_step=epoch)
+        writer.add_scalar("mAP/train", avg_mAP, global_step=epoch)
+    
+    return avg_loss, avg_mAP
 
 
 def validate_one_epoch(epoch, model, valid_loader, loss_func, writer, data_transforms=False, device='cpu', **kwargs):
@@ -173,12 +180,15 @@ def validate_one_epoch(epoch, model, valid_loader, loss_func, writer, data_trans
             predictions = logits_to_blood_vessel_mask(logits)
             blood_vessel_gt = (y == 2).type(torch.long)
             mAP_value = mAP(predictions, blood_vessel_gt, **kwargs)
-            writer.add_scalar("Loss/valid", loss.item(), global_step=epoch)
-            writer.add_scalar("mAP/valid", mAP_value, global_step=epoch)
             total_loss += loss.item()
             total_mAP += mAP_value
 
-    return total_loss / data_size, total_mAP / data_size
+    avg_loss = total_loss / data_size
+    avg_mAP = total_mAP / data_size
+    writer.add_scalar("Loss/valid", avg_loss, global_step=epoch)
+    writer.add_scalar("mAP/valid", avg_mAP, global_step=epoch)
+
+    return avg_loss, avg_mAP
     
 
 